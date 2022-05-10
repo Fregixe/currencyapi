e8f19423a90c18a839e16ba55c718c38d15dadba
Kiryl Zhuk <kizhofficial@gmail.com>
2022-05-10 20:14:22 +0200

    Small refactor

--- src/main/java/com/currency/api/configuration/SwaggerConfig.java
+++ src/main/java/com/currency/api/configuration/SwaggerConfig.java
@@ -7,6 +7,7 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
 public class SwaggerConfig {
+
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
